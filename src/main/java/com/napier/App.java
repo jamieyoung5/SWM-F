fabc7bf0e0851588015f13f91440e5981f0bea59
jamieyoung5 <jamie5young55@gmail.com>
2023-02-01 23:33:01 +0000

    fixed pom,added connection functionality, changed indent

--- src/main/java/com/napier/App.java
+++ src/main/java/com/napier/App.java
@@ -3,7 +3,8 @@ import java.io.*;
 import java.sql.*;
 
 public class App {
-    public static void main(String[] args) {
-
-    }
+            public static void main(String[] args) throws SQLException {
+                        DatabaseConnectionManager databaseConnectionManager = new DatabaseConnectionManager();
+                        databaseConnectionManager.CreateConnection();
+            }
 }
--- src/main/java/com/napier/DatabaseConnectionManager.java
+++ src/main/java/com/napier/DatabaseConnectionManager.java
@@ -1,22 +1,32 @@
 package com.napier;
 
+import java.io.*;
+import java.sql.*;
+
 public class DatabaseConnectionManager implements IDatabaseConnectionManager{
 
+            private static final String JDBC_SQL_DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    public DatabaseConnectionManager(){
+            public DatabaseConnectionManager(){
 
-    }
+            }
 
-    public void CreateConnection(){
-        VerifyDriver("");
-    }
+            public void CreateConnection() throws SQLException {
+                        VerifyDriver(JDBC_SQL_DRIVER);
+                        String url = "jdbc:mysql://localhost:3306/world";
 
-    private void VerifyDriver(String driverPath){
-        try{
-            Class.forName(driverPath);
-        } catch (ClassNotFoundException exception){
-            throw new RuntimeException("SQL driver not found");
-        }
-    }
+                        Connection connection = DriverManager.getConnection(url,"root","");
+                        Statement statement = connection.createStatement();
+                        ResultSet resultSet = statement.executeQuery("SELECT * FROM city");
+                        connection.close();
+            }
+
+            private void VerifyDriver(String driverPath){
+                        try{
+                                    Class.forName(driverPath);
+                        } catch (ClassNotFoundException exception){
+                                    throw new RuntimeException("SQL driver not found");
+                        }
+            }
 
 }
