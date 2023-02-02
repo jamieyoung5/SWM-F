6f9b87701df7aaf5df87418e759baf0c29457850
jamieyoung5 <Jamie.Young@n-able.com>
2023-02-02 16:57:48 +0000

    Add connection attempts

--- src/main/java/com/napier/DatabaseConnectionManager.java
+++ src/main/java/com/napier/DatabaseConnectionManager.java
@@ -1,29 +1,51 @@
 package com.napier;
 
-import java.io.*;
+import java.io.Console;
 import java.sql.*;
 
 public class DatabaseConnectionManager implements IDatabaseConnectionManager{
 
             private static final String JDBC_SQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+            private static final String DATABASE_USER = "root"; //TO-DO: put this in a config
+            private static final String DATABASE_PASS = "my-secret-pw"; //TO-DO: put this in a config
+            private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/world"; //TO-DO: put this somewhere else as well
+            private static final int MAX_CONNECTION_ATTEMPTS = 3;
+
 
             public DatabaseConnectionManager(){
 
             }
 
-            public void CreateConnection() throws SQLException {
-                        VerifyDriver(JDBC_SQL_DRIVER);
-                        String url = "jdbc:mysql://localhost:3306/world";
-
-                        Connection connection = DriverManager.getConnection(url,"root","");
-                        Statement statement = connection.createStatement();
-                        ResultSet resultSet = statement.executeQuery("SELECT * FROM city");
-                        connection.close();
+            public void ExecuteQuery(Connection connection) throws SQLException {
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM city");
+                System.out.println(resultSet.toString());
             }
 
-            private void VerifyDriver(String driverPath){
+            public Connection CreateConnection() {
+                        VerifyDriver();
+                        String url = "jdbc:mysql://localhost:3306/world";
+
+                        for(int connectionAttempts = 0; connectionAttempts < MAX_CONNECTION_ATTEMPTS; connectionAttempts++){
+                            ConnectionAttempt();
+                        }
+
+                        return connection;
+            }
+
+            private boolean ConnectionAttempt(){
+                try{
+                    Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER,DATABASE_PASS);
+                    return true;
+                }catch (SQLException exception){
+                    return false;
+                }
+
+            }
+
+            private void VerifyDriver(){
                         try{
-                                    Class.forName(driverPath);
+                                    Class.forName(DatabaseConnectionManager.JDBC_SQL_DRIVER);
                         } catch (ClassNotFoundException exception){
                                     throw new RuntimeException("SQL driver not found");
                         }
