07eca63cc435221e046100a5bfedbc5b430e930f
reynolds-benjamin <40626919@napier.ac.uk>
2023-02-21 11:41:35 +0000

    All capital cities scripts (Up to Q22) completed

--- src/main/sql/scripts.sql
+++ src/main/sql/scripts.sql
@@ -1,4 +1,4 @@
-/*All the countries in thw world, ordered by x*/
+/*All the countries in the world, ordered by x*/
 SELECT Name, Continent, Population FROM country ORDER BY Population desc;
 SELECT Name, Continent, Population FROM country WHERE Continent = 'Europe' ORDER BY Population desc ;
 SELECT Name, Region, Population FROM country WHERE Region = 'Caribbean' ORDER BY Population desc;
@@ -7,12 +7,28 @@ SELECT Name, Continent, Population FROM country WHERE Continent = 'Europe' ORDER
 SELECT Name, Region, Population FROM country WHERE Region = 'Caribbean' ORDER BY Population desc LIMIT UserInput();
 
 /*All the cities in the world ordered by x*/
-SELECT Name, CountryCode, Population from city ORDER BY Population desc;
+SELECT Name, CountryCode, Population FROM city ORDER BY Population desc;
 SELECT Name, Continent, Population FROM city WHERE Continent = 'Europe' ORDER BY Population desc;
-Select city.Name, city.Population, c.Region from city JOIN country c on city.CountryCode = c.Code where Region = 'Caribbean' ORDER BY Population desc;
-Select city.Name, city.Population, c.Name from city JOIN country c on city.CountryCode = c.Code where c.Name = 'Belgium' ORDER BY Population desc;
-Select city.Name, city.Population, city.District from city where city.District = 'Toscana' ORDER BY Population desc;
+SELECT city.Name, city.Population, c.Region FROM city JOIN country c on city.CountryCode = c.Code where Region = 'Caribbean' ORDER BY Population desc;
+SELECT city.Name, city.Population, c.Name FROM city JOIN country c on city.CountryCode = c.Code where c.Name = 'Belgium' ORDER BY Population desc;
+SELECT city.Name, city.Population, city.District FROM city where city.District = 'Toscana' ORDER BY Population desc;
 
+/*N cities in the world where N is provided by user*/
+SELECT Name, CountryCode, Population FROM city LIMIT UserInput();
+SELECT Name, Continent, Population FROM city WHERE Continent = 'Europe' UserInput();
+Select city.Name, city.Population, c.Region FROM city JOIN country c on city.CountryCode = c.Code where Region = 'Caribbean' UserInput();
+Select city.Name, city.Population, c.Name FROM city JOIN country c on city.CountryCode = c.Code where c.Name = 'Belgium' UserInput();
+Select city.Name, city.Population, city.District FROM city where city.District = 'Toscana' UserInput();
+
+/*Capital cities ordered by x*/
+SELECT c.Name, city.Name, city.Population FROM city JOIN country c on city.CountryCode = c.Code where city.ID = c.Capital ORDER BY city.Population desc;
+SELECT c.Name, city.Name, city.Population FROM city JOIN country c on city.CountryCode = c.Code where city.ID = c.Capital AND c.Continent = "Europe" ORDER BY city.Population desc;
+SELECT c.Name, city.Name, city.Population FROM city JOIN country c on city.CountryCode = c.Code where city.ID = c.Capital AND c.Region = "Caribbean" ORDER BY city.Population desc;
+
+/*N Capital cities where N is provided by user*/
+SELECT c.Name, city.Name, city.Population FROM city JOIN country c on city.CountryCode = c.Code where city.ID = c.Capital LIMIT UserInput();
+SELECT c.Name, city.Name, city.Population FROM city JOIN country c on city.CountryCode = c.Code where city.ID = c.Capital AND c.Continent = "Europe" LIMIT UserInput();
+SELECT c.Name, city.Name, city.Population FROM city JOIN country c on city.CountryCode = c.Code where city.ID = c.Capital AND c.Region = "Caribbean" LIMIT UserInput();
 
 /*Total population*/
 SELECT SUM(Population) AS "World Population" FROM country;
