6d547aaf1decf7a6aaf2ee3fb08225333969bdc9
jamieyoung5 <Jamie.Young@n-able.com>
2023-04-27 15:43:45 +0100

    Fixed sql queries and improved comments

--- src/main/sql/reports.sql
+++
@@ -1,8 +0,0 @@
-/*Country report */
-SELECT Code, Name, Continent, Region, Population, Capital FROM country where Name = 'UserInput()';
-
-/*City Report*/
-SELECT city.Name, country.Name, city.District, city.Population from city JOIN country on Code = city.CountryCode WHERE city.Name = 'UserInput()';
-
-/*Capital Report*/
-SELECT city.Name, country.Name, city.Population from city JOIN country on Code = city.CountryCode WHERE city.Name = 'UserInput()';
